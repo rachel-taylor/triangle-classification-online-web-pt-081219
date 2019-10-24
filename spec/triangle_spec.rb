88031184ea6974f80d9f0e37a8de2612482b11e1
Rachel T <rachmaybe@gmail.com>
2019-10-24 05:21:00 +0000

    Automatically backed up by Learn

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -1,3 +1,20 @@
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@rachel-taylor 
+1
+08alexisadorn/Flatiron-OO-Ruby
+ Code Issues 0 Pull requests 0 Projects 0 Wiki Security Insights
+Flatiron-OO-Ruby/triangle-classification-v-000/spec/triangle_spec.rb
+@alexisadorn alexisadorn oo-labs
+0675b55 on May 17, 2017
+64 lines (47 sloc)  2.05 KB
+  
 describe 'Triangle' do
 
   it 'knows that equilateral triangles have equal sides' do
@@ -41,23 +58,35 @@ describe 'Triangle' do
   end
 
   it 'knows that triangles with no size are illegal' do
-    expect{Triangle.new(0, 0, 0).kind}.to raise_error(Triangle::TriangleError)
+    expect{Triangle.new(0, 0, 0).kind}.to raise_error(TriangleError)
   end
 
   it 'knows that triangles with negative sides are illegal' do
-    expect{Triangle.new(3, 4, -5).kind}.to raise_error(Triangle::TriangleError)
+    expect{Triangle.new(3, 4, -5).kind}.to raise_error(TriangleError)
   end
 
   it 'knows that triangles violating triangle inequality are illegal' do
-    expect{Triangle.new(1, 1, 3).kind}.to raise_error(Triangle::TriangleError)
+    expect{Triangle.new(1, 1, 3).kind}.to raise_error(TriangleError)
   end
 
   it 'knows that triangles violating triangle inequality are illegal 2' do
-    expect{Triangle.new(2, 4, 2).kind}.to raise_error(Triangle::TriangleError)
+    expect{Triangle.new(2, 4, 2).kind}.to raise_error(TriangleError)
   end
 
   it 'knows that triangles violating triangle inequality are illegal 3' do
-    expect{Triangle.new(7, 3, 2).kind}.to raise_error(Triangle::TriangleError)
+    expect{Triangle.new(7, 3, 2).kind}.to raise_error(TriangleError)
   end
 
 end
+© 2019 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
